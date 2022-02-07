1b51edfe297a071257d2aadce8ee3bf7469161fa
thunderknight99 <67976182+thunderknight99@users.noreply.github.com>
2022-02-08 00:59:54 +0530

    changed database url

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(cors());
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "Ohdude123",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 });
 
